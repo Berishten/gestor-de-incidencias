e8f07d98a9553a7c1ce32e780574ce0c63517834
Berishten <vepaes439@gmail.com>
2024-07-04 23:12:58 -0400

    Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { BacklogColumnComponent } from "./components/backlog-column/backlog-column.component";
 import { DragAndDropComponent } from "./components/drag-and-drop/drag-and-drop.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, BacklogColumnComponent, DragAndDropComponent, SidebarComponent]
+    imports: [RouterOutlet, BacklogColumnComponent, DragAndDropComponent, SidebarComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'gestor-de-incidencias';
