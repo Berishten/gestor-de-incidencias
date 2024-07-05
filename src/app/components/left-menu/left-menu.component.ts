e86329b6a5e82deeedde116bf9d1c9650656c37f
Berishten <vepaes439@gmail.com>
2024-07-05 13:11:20 -0400

    Refactor left menu component

--- src/app/components/left-menu/left-menu.component.ts
+++ src/app/components/left-menu/left-menu.component.ts
@@ -14,11 +14,11 @@ export class LeftMenuComponent {
     { id: 1, nombre: 'Proyecto 1', boxes: ["Backlog", "Ready", "ToDo"] },
     { id: 2, nombre: 'Proyecto 2', boxes: ["Backlog", "Ready", "ToDo"] },
     { id: 3, nombre: 'Proyecto 3', boxes: ["Backlog", "Ready", "ToDo"] },
-    // { id: 4, nombre: 'Proyecto 4', boxes: ["Backlog", "Ready", "ToDo"] },
-    // { id: 5, nombre: 'Proyecto 5', boxes: ["Backlog", "Ready", "ToDo"] },
-    // { id: 6, nombre: 'Proyecto 6', boxes: ["Backlog", "Ready", "ToDo"] },
-    // { id: 7, nombre: 'Proyecto 7', boxes: ["Backlog", "Ready", "ToDo"] },
-    // { id: 8, nombre: 'Proyecto 8', boxes: ["Backlog", "Ready", "ToDo"] }
+    { id: 4, nombre: 'Proyecto 4', boxes: ["Backlog", "Ready", "ToDo"] },
+    { id: 5, nombre: 'Proyecto 5', boxes: ["Backlog", "Ready", "ToDo"] },
+    { id: 6, nombre: 'Proyecto 6', boxes: ["Backlog", "Ready", "ToDo"] },
+    { id: 7, nombre: 'Proyecto 7', boxes: ["Backlog", "Ready", "ToDo"] },
+    { id: 8, nombre: 'Proyecto 8', boxes: ["Backlog", "Ready", "ToDo"] }
   ];
   globalConfigService = inject(GlobalConfigService);
 
